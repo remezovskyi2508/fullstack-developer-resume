767ac38a3c69de31243739b59f407075ee2585a5
tatiana_bahrova <gyberman@gmail.com>
2024-09-08 12:37:41 +0300

    changed swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,12 +1,7 @@
 import Accordion from 'accordion-js';
 import Swiper from 'swiper/bundle';
 
-// import Swiper, { Autoplay, Navigation } from 'swiper';
-
-import 'swiper/css/navigation';
-
 const accordionBtn = document.querySelectorAll('.about-me-btn');
-
 const container = document.querySelector('.accordion-container');
 
 const accordion = new Accordion(container, {
@@ -27,43 +22,38 @@ const accordion = new Accordion(container, {
 });
 
 const swiper = new Swiper('.swiper', {
+  setWrapperSize: true,
   loop: true,
   speed: 500,
   simulateTouch: true,
   spaceBetween: 0,
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
   },
-  slidesPerView: 2,
-  spaceBetween: 10,
+
   keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
   breakpoints: {
-    // when window width is >= 320px
     375: {
-      slidesPerView: 2,
+      slidesPerView: 'auto',
       spaceBetween: 0,
     },
-    // when window width is >= 480px
+
     768: {
-      slidesPerView: 3,
+      slidesPerView: 'auto',
       spaceBetween: 0,
     },
-    // when window width is >= 640px
+
     1400: {
-      slidesPerView: 4,
+      slidesPerView: 'auto',
       spaceBetween: 0,
     },
   },
+  slidesPerView: 'auto',
+  spaceBetween: 0,
 });
-swiper.allowSlideNext = true;
-
-// document.querySelectorAll('.swiper-slide').forEach(slide => {
-//   slide.style.setProperty('width', '130px', 'important');
-// });
 
 accordionBtn.forEach(btn => {
   btn.addEventListener('click', event => {
